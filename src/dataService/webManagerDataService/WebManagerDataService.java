51b678418579e98618939e1b3d756ca3faceee53
CharlesFeng <cuihua@cuihua.local>
2016-10-16 22:38:18 +0800

    ..

--- src/dataService/webManagerDataService/WebManagerDataService.java
+++ src/dataService/webManagerDataService/WebManagerDataService.java
@@ -1,5 +1,13 @@
 package dataService.webManagerDataService;
 
+import po.WebManagerPO;
+import utilities.ResultMessage;
+
 public interface WebManagerDataService {
 
+	public ResultMessage getSingle (String webManagerID);
+
+	public ResultMessage add(WebManagerPO newWebManagerPO);
+
+	public ResultMessage modify (WebManagerPO webManagerPO);
 }
