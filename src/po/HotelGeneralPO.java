a690d2dc7b8885d1c0ef50320265c9154e6d7d09
GY15 <619903532@qq.com>
2016-10-16 16:21:59 +0800

    general更改

---
+++ src/po/HotelGeneralPO.java
@@ -0,0 +1,58 @@
+package po;
+
+public class HotelGeneralPO {
+	//	酒店名称
+	private String hotelName;
+	//	酒店城市
+	private String city;
+	//	酒店商圈
+	private String cycle;
+	//	酒店星级
+	private String level;
+	//	评分
+	private int score;
+
+	public HotelGeneralPO(String hotelName, String city, String cycle, String level, int score) {
+		super();
+		this.hotelName = hotelName;
+		this.city = city;
+		this.cycle = cycle;
+		this.level = level;
+		this.score = score;
+	}
+	
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCycle() {
+		return cycle;
+	}
+
+	public void setCycle(String cycle) {
+		this.cycle = cycle;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	
+}
