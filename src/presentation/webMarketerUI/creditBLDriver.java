88d6844bf556fa3e4723a592435e030a8adcae74
CharlesFeng <cuihua@cuihua.local>
2016-10-16 19:09:38 +0800

    修改

--- src/presentation/webMarketerUI/creditBLDriver.java
+++ src/presentation/webMarketerUI/creditBLDriver.java
@@ -2,8 +2,9 @@ package presentation.webMarketerUI;
 
 import businessLogic.creditBL.CreditBLService_Stub;
 
-public class creditBLDriver {
+public class CreditBLDriver {
 
-	CreditBLService_Stub creditBLService_Stub = new CreditBLService_Stub("1", "1", 1, "1", "1");
+	CreditBLService_Stub creditBLService_Stub = new CreditBLService_Stub
+			("0000000001", "L2", "1995/1/1", "NJU", "Charles", "bhsab", "123456", "13000001111", "500", "2016", "123411111111", 0, "undo");
 	
 }
