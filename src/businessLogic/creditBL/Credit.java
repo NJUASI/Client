b8f47f3bd9ed3c8a27df2f0f4e66e860b5d0fa53
Byron Dong <2104869365@qq.com>
2016-11-05 17:31:08 +0800

    添加domain

---
+++ src/businessLogic/creditBL/Credit.java
@@ -0,0 +1,17 @@
+package businessLogic.creditBL;
+
+import utilities.ResultMessage;
+import vo.CreditVO;
+
+public class Credit {
+
+	public ResultMessage charge(int chargeNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CreditVO getBasicInfo(String ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
