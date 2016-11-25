e0a20a59d2396242dabb326113b7ab2ff04f0a12
GY15 <619903532@qq.com>
2016-11-25 18:47:43 +0800

    所有界面详细跳转实现

--- src/main/java/presentation/hotelWorkerUI/controller/HotelController.java
+++ src/main/java/presentation/hotelWorkerUI/controller/HotelController.java
@@ -1,5 +1,30 @@
 package presentation.hotelWorkerUI.controller;
 
-public class HotelController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
+public class HotelController {
+	@FXML
+	private Pane hotelModifyPane;
+	@FXML
+	private Pane hotelInfoPane;
+
+	@FXML
+	protected void modify() {
+		hotelModifyPane.setVisible(true);
+		hotelInfoPane.setVisible(false);
+	}
+
+	@FXML
+	protected void save() {
+		hotelModifyPane.setVisible(false);
+		hotelInfoPane.setVisible(true);
+	}
+	
+
+	@FXML
+	protected void cancel() {
+		hotelModifyPane.setVisible(false);
+		hotelInfoPane.setVisible(true);
+	}
 }
--- src/main/java/presentation/webMarketerUI/controller/OrderController.java
+++ src/main/java/presentation/webMarketerUI/controller/OrderController.java
@@ -1,5 +1,54 @@
 package presentation.webMarketerUI.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 public class OrderController {
+	
+	@FXML
+	private Pane orderCheck,orderDetail,searchPane;
+	@FXML
+	private Button back1,back2 ;
+	
+	@FXML
+	protected void searchOneOrder() {
+		back1.setVisible(false);
+		back2.setVisible(true);
+		orderDetail.setVisible(true);
+		searchPane.setVisible(false);
+	}
+
+	@FXML
+	protected void searchDateOrder() {
+		orderCheck.setVisible(true);
+		searchPane.setVisible(false);
+	}
+
+	@FXML
+	protected void cancelCheck() {
+		orderCheck.setVisible(false);
+		searchPane.setVisible(true);
+	}
+
+	@FXML
+	protected void OrderDetail1() {
+		back1.setVisible(true);
+		back2.setVisible(false);
+		orderDetail.setVisible(true);
+		orderCheck.setVisible(false);
+	}
+
+
+	@FXML
+	protected void cancelDetail1() {
+		orderDetail.setVisible(false);
+		orderCheck.setVisible(true);
+	}
+	@FXML
+	protected void cancelDetail2() {
+		orderDetail.setVisible(false);
+		searchPane.setVisible(true);
+	}
 
 }
