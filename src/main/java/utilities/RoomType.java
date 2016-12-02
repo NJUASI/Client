a85de4820330c24afdf5d4783dff5c2939b6772b
Byron Dong <2104869365@qq.com>
2016-12-03 00:26:51 +0800

    把vo,po提出来

--- src/main/java/utilities/RoomType.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-import java.io.Serializable;
-
-public enum RoomType implements Serializable{
-	SINGLE_BED, DOUBLE_BED, TRIPLE_BED, DELUXE, PREMIER, AMBASSADOR, IMPERIAL;
-}
--- src/main/java/utilities/UserType.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-import java.io.Serializable;
-
-public enum UserType implements Serializable{
-   GUEST, HOTEL_WORKER, WEB_MARKETER, WEB_MANAGER;
-}
